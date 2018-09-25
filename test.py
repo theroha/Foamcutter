33f79de7b4afcb51635e4dae68bbdd92d8d92a81
Jared Strain <jared.strain@yahoo.com>
2018-09-25 08:07:48 -0500

    Update test.py
    Simplify clickCheck

--- test.py
+++ test.py
@@ -7,11 +7,15 @@ from pathlib import Path
 
 pygame.init()
 
+# Set UI
 size = width, height = [640, 489]
 white = (255,255,255)
 
 screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
+'''
+
+'''
 class ICON:
     def __init__(self, image, svgFile):
         self.image = pygame.image.load(image)
@@ -19,7 +23,8 @@ class ICON:
         self.svgFile = svgFile
 
     def clickCheck(self, mouse):
-        if mouse[0] >= self.rect.topleft[0] and mouse[1] >= self.rect.topleft[1] and mouse[0] <= self.rect.bottomright[0] and mouse[1] <= self.rect.bottomright[1]:
+        #if mouse[0] >= self.rect.topleft[0] and mouse[1] >= self.rect.topleft[1] and mouse[0] <= self.rect.bottomright[0] and mouse[1] <= self.rect.bottomright[1]:
+        if self.collidepoint(mouse): 
             run(['python', '/usr/share/inkscape/extensions/makeict_foamcutter.py', self.svgFile])
         else:
             return False
@@ -28,6 +33,7 @@ class ICON:
 
 swirl = ICON('/home/pi/test.png', '/home/pi/test.svg')
 
+# Main loop
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
